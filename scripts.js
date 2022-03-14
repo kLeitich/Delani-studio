545bf9b62e709752314efba9de6cbe2a4540f3e3
kLeitich <kevleitich@gmail.com>
2022-03-14 22:54:02 +0300

    adding assets

--- scripts.js
+++ scripts.js
@@ -1,37 +1,29 @@
-// let columns = ["design1", "development1", "product1"];
-
-// columns.forEach((col) => {
-//   $(`#${col}-col`).click((e) => {
-//     //prevent the page from going to the top
-//     e.preventDefault();
-
-//     $(`.${col}-info`).toggle();
-//     $(`#${col}-development img`).toggle();
-//     $(`#${col}-development p`).toggle();
-//   });
-// });
-
-
-
-// let fName = document.getElementById("name").value;
-// let email = document.getElementById("email").value;
-// let message = document.getElementById("message").value;
-
-// console.log(fName);
-// console.log(email);
-// console.log(message);
-
+//toggle for design
 $('.infode').hide();
 $('#design,.infode').click(function(){
 		$('#design,.infode').toggle();
 });
-
+//toggle for development
 $('.infod').hide();
 $('#development,.infod').click(function(){
 		$('#development,.infod').toggle();
 });
-
+//toggle for product
 $('.infop').hide();
 $('#product,.infop').click(function(){
 		$('#product,.infop').toggle();
 });
+
+
+//hover effects on portfolio
+
+
+//get data from form
+
+let fName = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let message = document.getElementById("message").value;
+
+console.log(fName);
+console.log(email);
+console.log(message);
