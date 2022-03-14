9acd519f913608d011768c07e1baf99c28be4609
kLeitich <kevleitich@gmail.com>
2022-03-14 15:39:15 +0300

    adding js that work in toggliing between two divs in what to do section

--- scripts.js
+++ scripts.js
@@ -1,22 +1,37 @@
-let columns = ["design1", "development1", "product1"];
+// let columns = ["design1", "development1", "product1"];
 
-columns.forEach((col) => {
-  $(`#${col}-col`).click((e) => {
-    //prevent the page from going to the top
-    e.preventDefault();
+// columns.forEach((col) => {
+//   $(`#${col}-col`).click((e) => {
+//     //prevent the page from going to the top
+//     e.preventDefault();
 
-    $(`.${col}-info`).toggle();
-    $(`#${col}-development img`).toggle();
-    $(`#${col}-development p`).toggle();
-  });
+//     $(`.${col}-info`).toggle();
+//     $(`#${col}-development img`).toggle();
+//     $(`#${col}-development p`).toggle();
+//   });
+// });
+
+
+
+// let fName = document.getElementById("name").value;
+// let email = document.getElementById("email").value;
+// let message = document.getElementById("message").value;
+
+// console.log(fName);
+// console.log(email);
+// console.log(message);
+
+$('.infode').hide();
+$('#design,.infode').click(function(){
+		$('#design,.infode').toggle();
 });
 
+$('.infod').hide();
+$('#development,.infod').click(function(){
+		$('#development,.infod').toggle();
+});
 
-
-let fName = document.getElementById("name").value;
-let email = document.getElementById("email").value;
-let message = document.getElementById("message").value;
-
-console.log(fName);
-console.log(email);
-console.log(message);
+$('.infop').hide();
+$('#product,.infop').click(function(){
+		$('#product,.infop').toggle();
+});
