9155cb1b18d2fe23aac0a6e09ba5f23a34ead71b
kLeitich <kevleitich@gmail.com>
2022-03-15 02:52:01 +0300

    adding functions to validate name,email,message and alert on error

--- scripts.js
+++ scripts.js
@@ -37,12 +37,82 @@ let portfolios = [
       }
     );
   });
+
+
+
+//validation function
+
+const correctEmail = (email)=> {
+    var validEmail =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!email.match(validEmail)) {
+    alert("Enter a valid email");
+  }
+}
+const CorrectNameMessage =(name,message)=>{
+    if (name ==""){
+        alert("Required input, Input your first Name")
+    }else if (message==""){
+        alert("Required input, input your  message")
+    }
+}
+
 //get data from form
+let form = document.getElementById('userInput');
 
-let fName = document.getElementById("name").value;
-let email = document.getElementById("email").value;
-let message = document.getElementById("message").value;
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let fName = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    correctEmail(email);
+    CorrectNameMessage(fName,message);
 
-console.log(fName);
-console.log(email);
-console.log(message);
+     if (fName != "" && email != "" && message != "") {
+        alert("Message sent")
+        $("#userInput").trigger("reset");
+      };
+
+
+});
+
+
+//mail api
+
+
+// fetch('https://api.sendgrid.com/v3/mail/send')
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err) );
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+
+// const payload = {
+//     userId: 1,
+//     id: 1,
+//     title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//     body: "quia et suscipit\nsuscipit recusandae consequuntur
+
+
+
+// fetch('https://api.sendgrid.com/v3/mail/send/', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         'email': 'user@example.com',
+//         'message': 'Your message has been received.',
+        
+//     }),
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// }).then(response => {
+//     console.log(response.status);
+//       return response.json()
+//   })
+//   .then(data => console.log(data))
+//     .catch ((err) => {
+//         console.log(err);
+// })
+
